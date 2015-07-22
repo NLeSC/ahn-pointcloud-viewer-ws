e9a028c0c6b02c0b568719b1cf10a91939285f78
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-07-22 16:33:36 +0200

    Dont use underscore var names

--- src/main/java/nl/esciencecenter/ahn/pointcloud/db/PotreeDistancesDOA.java
+++ src/main/java/nl/esciencecenter/ahn/pointcloud/db/PotreeDistancesDOA.java
@@ -7,10 +7,10 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 public interface PotreeDistancesDOA {
     @SqlQuery("SELECT level " +
             "FROM potree_dist " +
-            "WHERE :numpoints_raw*ratio <= :maxpoints " +
-            "ORDER BY POWER(:numpoints_raw * ratio - :maxpoints, 2)" +
+            "WHERE :rawpoints * ratio <= :maxpoints " +
+            "ORDER BY POWER(:rawpoints * ratio - :maxpoints, 2)" +
             "LIMIT 1")
-    int getLevel(@Bind("numpoints_raw") long numpoints_raw, @Bind("maxpoints") long maxpoints);
+    int getLevel(@Bind("rawpoints") long rawpoints, @Bind("maxpoints") long maxpoints);
 
     @SqlQuery("SELECT MAX(level) FROM potree_dist")
     int getMaxLevel();
