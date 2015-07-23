885f9c87edd918e487a097a2fe780d55a3d17c7c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-07-23 17:06:53 +0200

    Use floor instead of casting to bigint.

--- src/main/java/nl/esciencecenter/ahn/pointcloud/db/RawExtentsDOA.java
+++ src/main/java/nl/esciencecenter/ahn/pointcloud/db/RawExtentsDOA.java
@@ -7,7 +7,7 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 
 public interface RawExtentsDOA {
     @SqlQuery("SELECT "
-            + "  SUM(LEAST(numberpoints, (numberpoints * (ST_Area(qgeom) /ST_Area(geom)))::bigint)) AS numpoints_raw "
+            + "  FLOOR(SUM(LEAST(numberpoints, (numberpoints * (ST_Area(qgeom) /ST_Area(geom)))))) AS numpoints_raw "
             + "FROM "
             + "  extent_raw, "
             + "  (SELECT ST_SetSRID(ST_MakeBox2D(ST_Point(:left, :bottom),ST_Point(:right, :top)), :srid) AS qgeom) AS B "
